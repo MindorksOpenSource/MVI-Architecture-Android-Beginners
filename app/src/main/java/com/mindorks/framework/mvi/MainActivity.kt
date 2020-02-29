897201bb98f93b2bfaa2ce369b7ed511a0682585
Himanshu Singh <hello2himanshusingh@gmail.com>
2020-02-29 14:01:12 +0530

    Manifest Update

--- app/src/main/java/com/mindorks/framework/mvi/MainActivity.kt
+++ app/src/main/java/com/mindorks/framework/mvi/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mindorks.mvi
+package com.mindorks.framework.mvi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mindorks.framework.mvi.R
 
 class MainActivity : AppCompatActivity() {
 
