596ad453a6725c2e87949d470a64e9ff9c86dd22
Himanshu Singh <hello2himanshusingh@gmail.com>
2020-06-16 09:35:52 +0530

    Update Project

--- app/src/main/java/com/mindorks/framework/mvi/ui/main/viewstate/MainState.kt
+++ app/src/main/java/com/mindorks/framework/mvi/ui/main/viewstate/MainState.kt
@@ -7,6 +7,6 @@ sealed class MainState {
     object Idle : MainState()
     object Loading : MainState()
     data class Users(val user: List<User>) : MainState()
-    data class Error(val error: String) : MainState()
+    data class Error(val error: String?) : MainState()
 
 }
