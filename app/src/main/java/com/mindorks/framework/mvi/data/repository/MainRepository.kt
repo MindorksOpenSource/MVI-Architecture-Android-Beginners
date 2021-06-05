43386c981ee032df7b202997efe3720115e6812f
Ali Rezaei <ali.rezaei09@gmail.com>
2021-06-05 16:37:31 +0430

    Modificarion

--- app/src/main/java/com/mindorks/framework/mvi/data/repository/MainRepository.kt
+++ app/src/main/java/com/mindorks/framework/mvi/data/repository/MainRepository.kt
@@ -2,9 +2,10 @@ package com.mindorks.framework.mvi.data.repository
 
 import com.mindorks.framework.mvi.data.model.User
 import com.mindorks.framework.mvi.ui.main.viewstate.MainState
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.StateFlow
-import java.util.*
 
+@ExperimentalCoroutinesApi
 interface MainRepository {
 
     val state: StateFlow<MainState<List<User>>>
